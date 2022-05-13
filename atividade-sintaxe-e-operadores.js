5dd9d07ed45ccde4aa34a0f2b1f387a58af90d50
GustaKowai <gusta.kowai@gmail.com>
2022-05-13 20:52:33 -0300

    Adiciona comentários ao código.

--- atividade-sintaxe-e-operadores.js
+++ atividade-sintaxe-e-operadores.js
@@ -1,11 +1,11 @@
 //essa atividade irá comparar dois números e retornar se são iguais, e se a soma é menor do que 10 e/ou menor do que 20.
 function comparar (numero1,numero2){
-  let igual = numero1 == numero2 ? '' : 'não';
-  if (numero1 + numero2  > 20){
+  let igual = numero1 == numero2 ? '' : 'não'; //treinar o uso do operador ternário.
+  if (numero1 + numero2  > 20){   // Aqui compara se é maior que 20. Por lógica, se for maior que 20 também é maior que 10.
      comp20 = 'maior';
      comp10 = 'maior';
   }else{
-      if (numero1 + numero2 > 10){
+      if (numero1 + numero2 > 10){ //Se entrar aqui, com certeza é menor que 20, então é só comparar com 10.
         comp20 = 'menor';
         comp10 = 'maior';
       }else{
