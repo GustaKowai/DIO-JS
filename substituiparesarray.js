ca41492fd56b4fdee1f900e0b6d7c90525782718
GustaKowai <gusta.kowai@gmail.com>
2022-05-14 17:34:36 -0300

    Faz o exercicio muito parecido com o da aula.

---
+++ substituiparesarray.js
@@ -0,0 +1,17 @@
+function tirapares (array){
+  if(!array) return -1
+  if(!array.length) return -1
+
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++){
+    if (array[i] == 0){
+      
+    }else if(array[i]%2 == 0){
+      array[i] = 0;
+    }
+  }
+  return array;
+}
+
+let arr = [1,2,3,4,5,6];
+console.log(tirapares(arr));
