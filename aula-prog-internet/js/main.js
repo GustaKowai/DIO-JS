d306f366c8795c5559f3c989df1e99ff91d4a381
GustaKowai <gusta.kowai@gmail.com>
2022-05-20 21:02:37 -0300

    Aulas de js para internet

---
+++ aula-prog-internet/js/main.js
@@ -0,0 +1,80 @@
+
+//Primeira aula:
+/*var d = new Date;
+alert(d.getMonth());
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getSeconds());
+*/
+
+/*var count;
+for(count = 0; count<=5; count++){
+  alert(count);
+}
+*/
+/*
+var count = 0;
+while (count<=5){
+  console.log(count);
+  count ++;
+};
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+  alert("Maior de idade");
+}else{
+  alert("menor de idade");
+}
+;
+*/
+
+
+//Segunda aula:
+/*var frutas = [{
+  nome:"maça",
+  cor:"vermelha"
+},
+{
+  nome:"uva",
+  cor:"roxa"
+},
+{
+  nome:"limão",
+  cor:"verde"
+},
+];
+console.log(frutas);
+*/
+/*var fruta = {
+  nome:"maça",
+  cor:"vermelha"
+};
+
+console.log(fruta.cor);*/
+
+/*var lista = ["maçã", "pêra", "laranja"];
+console.log(lista);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" "));*/
+
+
+
+//Primeira aula:
+/*var nome = "Gustavo Gregorio";
+var idade = 28;
+var idade2 = 10;
+var frase = "Japão é nice."
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"))*/
