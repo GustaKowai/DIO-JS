dea4af7dc4496bd46790ae8d3480dbadbdd22720
GustaKowai <gusta.kowai@gmail.com>
2022-06-07 15:02:49 -0300

    Exercício de orientação objeto em js da DIO.

---
+++ conta-bancaria/conta-bancaria.js
@@ -0,0 +1,70 @@
+class ContaBancaria {
+  constructor(agencia,numero,tipo){
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this._saldo = 0;
+  }
+
+  get saldo(){
+    return this._saldo;
+  }
+
+  set saldo(valor){
+    this._saldo = valor;
+  }
+
+  sacar(valor){
+    if(valor > this._saldo){
+      return "Saldo insuficiente."
+    }
+
+    this._saldo = this._saldo -valor;
+    return this._saldo;
+  }
+
+  depositar(valor){
+    this._saldo = this._saldo + valor;
+    return this._saldo;
+  }
+}
+
+class ContaCorrente extends ContaBancaria{
+  constructor(agencia,numero, cartaoCredito){
+    super(agencia,numero);
+    this.type = 'corrente';
+    this._cartaoCredito = cartaoCredito;
+  }
+
+  get cartaoCredito(){
+    return this._cartaoCredito;
+  }
+
+  set cartaoCredito(valor){
+    this._cartaoCredito = valor;
+  }
+}
+
+class ContaPoupança extends ContaBancaria{
+  constructor(agencia,numero){
+    super(agencia,numero);
+    this.type = 'poupança';
+  }
+}
+class ContaUniversitaria extends ContaBancaria{
+  constructor(agencia,numero){
+    super(agencia,numero);
+    this.type = 'universitaria';
+  }
+
+  sacar(valor){
+    if(valor > 500){
+      return "Operação negada"
+    }
+    if(valor > this._saldo){
+      return "Saldo insuficiente."
+    }
+    this._saldo = this._saldo - valor;
+    return this._saldo;
+  }
+}
