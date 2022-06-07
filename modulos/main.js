5f39d005975688c8b3fc4ee1ed790ffc8d2c199a
GustaKowai <gusta.kowai@gmail.com>
2022-06-07 16:10:00 -0300

    Adiciona a atividade de módulos

---
+++ modulos/main.js
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade,mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Gustavo", 28));
+console.log(mostraCidade("Gustavo","São Carlos"));
+console.log(mostraHobby("Gustavo", "Ler mangás"));
