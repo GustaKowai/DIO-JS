ef37c9206999e44d82849499e587be6ae0d2228d
GustaKowai <gusta.kowai@gmail.com>
2022-06-15 14:40:40 -0300

    add aulas

--- dio-typescript/index.ts
+++ dio-typescript/index.ts
@@ -1,1 +1,47 @@
 console.log('TypeScript')
+
+function soma(a: number, b: number){
+  return a + b;
+}
+
+soma(3,4); 
+
+//aula interface:
+
+interface IAnimal{
+  nome: string;
+  tipo: 'terrestre'|'aquático';
+  domestico: boolean;
+}
+
+interface IFelino extends IAnimal{
+  visaonoturna: boolean;
+}
+
+const animal: IAnimal = {
+  nome:'Tartauga',
+  tipo: 'aquático',
+  domestico: true,
+}
+
+const gato: IFelino = {
+  nome:'Atsushi',
+  tipo:'terrestre',
+  domestico: true,
+  visaonoturna: true,
+}
+
+//aula type:
+
+interface ICanino extends IAnimal{
+  porte: 'pequeno'|'médio'|'grande';
+}
+
+type TDomestico = IFelino | ICanino
+
+const animaldomestico : TDomestico = {
+  domestico: true,
+  nome: 'cachorro',
+  porte: 'médio',
+  tipo: 'terrestre',
+}
